509094deee11dafea827312c672a39b0349cb17a
Dan Shreim <dan@snapjay.com>
2018-01-09 13:44:36 -0500

    mend

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,70 +1,69 @@
-export namespace Podium {
-    export interface PodiumSDK {
-        setting: Settings;
-        Paginator: Paginator;
-        auth: Auth;
-        lrg: LRG;
-        incentive: Incentive;
-        profile: Profile;
-        terms: Terms;
-    }
 
-    export interface Auth {
-        constructor(settings: Settings)
-        login(login: string, password: string, programId: number): Promise;
-        basicAuth(token: string): void;
-        logout(token: string): Promise;
-    }
+export interface PodiumSDK {
+    setting: Settings;
+    Paginator: Paginator;
+    auth: Auth;
+    lrg: LRG;
+    incentive: Incentive;
+    profile: Profile;
+    terms: Terms;
+}
 
-    export interface Incentive {
-        constructor(settings: Settings)
-        getBalance(): Promise;
-        getTransactions(paginator: Paginator): Promise;
-    }
+export interface Auth {
+    constructor(settings: Settings)
+    login(login: string, password: string, programId: number): Promise;
+    basicAuth(token: string): void;
+    logout(token: string): Promise;
+}
 
-    export interface LRG {
-        constructor(settings: Settings)
-        get(redirectUrl: string): Promise;
-        redirect(redirectUrl: string): boolean;
-    }
+export interface Incentive {
+    constructor(settings: Settings)
+    getBalance(): Promise;
+    getTransactions(paginator: Paginator): Promise;
+}
 
-    export interface Profile {
-        constructor(settings: Settings)
-        get(redirectUrl: string): Promise;
-    }
+export interface LRG {
+    constructor(settings: Settings)
+    get(redirectUrl: string): Promise;
+    redirect(redirectUrl: string): boolean;
+}
 
-    export interface Terms {
-        constructor(settings: Settings)
-        get(redirectUrl: string): Promise;
-        accept(id: number): Promise;
-    }
+export interface Profile {
+    constructor(settings: Settings)
+    get(redirectUrl: string): Promise;
+}
 
-    export interface Paginator {
-        constructor(settings: Settings)
-        setContext(ctx: object): Paginator;
-        setPage(page: number): Paginator;
-        setPerPage(perPage: number): Paginator;
-        setSortField(sortField: number): Paginator;
-        setSortDirection(direction: SORT_DIRECTION): Paginator;
-        setSortDesc(direction: boolean): Paginator;
-        toParams(): {
-            page: number
-            count: number
-            sort_field: string
-            sort_direction: SORT_DIRECTION
-        };
-    }
+export interface Terms {
+    constructor(settings: Settings)
+    get(redirectUrl: string): Promise;
+    accept(id: number): Promise;
+}
 
-    export interface Settings {
-        endpoint: string;
-        catchError: Function;
-        perPage: number;
-        sortField: string;
-        sortDirection: SORT_DIRECTION;
-    }
+export interface Paginator {
+    constructor(settings: Settings)
+    setContext(ctx: object): this;
+    setPage(page: number): this;
+    setPerPage(perPage: number): this;
+    setSortField(sortField: number): this;
+    setSortDirection(direction: SORT_DIRECTION): this;
+    setSortDesc(direction: boolean): this;
+    toParams(): {
+        page: number
+        count: number
+        sort_field: string
+        sort_direction: SORT_DIRECTION
+    };
+}
 
-    export enum SORT_DIRECTION {
-        ASC = 'asc',
-        DESC = 'desc'
-    }
+export interface Settings {
+    endpoint: string;
+    catchError: Function;
+    perPage: number;
+    sortField: string;
+    sortDirection: SORT_DIRECTION;
+}
+
+export enum SORT_DIRECTION {
+    ASC = 'asc',
+    DESC = 'desc'
 }
