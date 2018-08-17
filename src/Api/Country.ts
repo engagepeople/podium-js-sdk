3a4f16af3712fc86f7af5e75ff8dfe895a128bee
s <agafonov.sergey@gmail.com>
2018-08-17 11:45:37 -0400

    simplified country/region api

--- src/Api/Country.ts
+++ src/Api/Country.ts
@@ -1,10 +1,24 @@
+import {ICountry, ICountryFilter, IPodiumPromise} from '../../types'
 import {Resource} from '../Podium/Resource'
 import {Settings} from '../Podium/Settings'
+import {Paginator} from '../Podium/Paginator'
+import {Filter} from '../Podium/Filter'
 
 export class Country extends Resource {
 
     constructor(settings: Settings) {
         super(settings)
         this.SetResource('country')
+    }
+
+    // tslint:disable-next-line:max-line-length
+    public List<F = ICountryFilter, T = ICountry>(program: string | Filter<F> | Paginator, paginator?: Paginator): IPodiumPromise<T[]>
+    // tslint:disable-next-line:no-any
+    public List(programSlug: string, ...args: any[]): IPodiumPromise<ICountry[]> {
+        if (typeof programSlug === 'string') {
+            const filter = new Filter<ICountryFilter>({ program_slug: programSlug })
+            return super.List<ICountryFilter, ICountry>(filter, ...args)
+        }
+        return super.List(programSlug, ...args)
     }
 }
--- src/Api/Region.ts
+++ src/Api/Region.ts
@@ -1,10 +1,24 @@
+import {IPodiumPromise, IRegion, IRegionFilter} from '../../types'
 import {Resource} from '../Podium/Resource'
 import {Settings} from '../Podium/Settings'
+import {Paginator} from '../Podium/Paginator'
+import {Filter} from '../Podium/Filter'
 
 export class Region extends Resource {
 
     constructor(settings: Settings) {
         super(settings)
         this.SetResource('region')
+    }
+
+    // tslint:disable-next-line:max-line-length
+    public List<F = IRegionFilter, T = IRegion>(country: string | Filter<F> | Paginator, paginator?: Paginator): IPodiumPromise<T[]>
+    // tslint:disable-next-line:no-any
+    public List(countryId: string, ...args: any[]): IPodiumPromise<IRegion[]> {
+        if (typeof countryId === 'string') {
+            const filter = new Filter<IRegionFilter>({ country_id: countryId })
+            return super.List<IRegionFilter, IRegion>(filter, ...args)
+        }
+        return super.List(countryId, ...args)
     }
 }
